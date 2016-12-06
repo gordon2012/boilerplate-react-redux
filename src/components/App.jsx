e26237aacbeb58f04e4e0d2bf841e6cea9792d87
Gordon Doskas <gordon2012@users.noreply.github.com>
2016-12-05 19:00:00 -0800

    Refactor out cruft from create-react-app (Closes #3)

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Boilerplate: React Redux</h1>
+        </header>
+        <p>
+          A starting point for applications powered by React and Redux.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
